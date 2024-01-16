afaf841b59cde9d1205bc162612b77a65aa43d00
armandopires <armandopires@pursuit.org>
2024-01-16 16:13:18 -0500

    Made fetch.js and data fetching

--- src/components/StudentsIndex.jsx
+++ src/components/StudentsIndex.jsx
@@ -1,6 +1,20 @@
 import { useState, useEffect } from "react"
+import { getAllStudents } from "../api/fetch";
 
 const StudentsIndex = () => {
+  const [allstudents, setAllStudents] = useState([]);
+
+  useEffect (()=> {
+    getAllStudents()
+    .then((data) => {
+      console.log(data)
+      setAllStudents(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }, [])
+
   return (
     <div>StudentsIndex</div>
   )
