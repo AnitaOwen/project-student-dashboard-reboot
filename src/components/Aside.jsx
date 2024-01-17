af4fcebac9c837ac8e8a29bb589297b30da56188
Jazon Younge <jay@Jazons-Air.lan>
2024-01-17 13:15:02 -0500

    added useParams

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Aside = ({ allStudents }) => {
+  const { cohortCode } = useParams();
   const studentCohort = allStudents.map((student) => {
     return student.cohort.cohortCode;
   });
@@ -10,7 +12,21 @@ const Aside = ({ allStudents }) => {
     return student.cohort.cohortCode === "Winter2025";
   });
   console.log(winter2025);
-  return <div>Aside</div>;
+  return (
+    <div>
+      <ul>
+        <li>All Students</li>
+        <li>Winter 2025</li>
+        <li>Spring 2025</li>
+        <li>Summer 2025</li>
+        <li>Fall 2025</li>
+        <li>Winter 2026</li>
+        <li>Spring 2026</li>
+        <li>Summer 2026</li>
+        <li>Fall 2026</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Aside;
