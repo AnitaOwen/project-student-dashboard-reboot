e4f29875e6eff0a13f6cf37ef877216e51c3f5ec
AnitaOwen <anitaowen@pursuit.org>
2024-01-18 12:28:02 -0500

    added cohort title change functionality to student list component

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -4,14 +4,21 @@ import Student from "./Student";
 const StudentList = ({allStudents, selectedCohort}) => {
   const count = allStudents.length
 
+  let cohortTitle;
+  if(selectedCohort){
+    const titleArray = selectedCohort.split("")
+    const season = titleArray.filter((char) => isNaN(+char) === true).join("")
+    const year = titleArray.filter((char) => isNaN(+char) === false).join("")
+    cohortTitle = `${season} ${year}`
+  }
 
   return (
     <>
-    <h2>{selectedCohort ? selectedCohort : "All Students"} ({count})</h2>
+    <h2>{selectedCohort ? cohortTitle : "All Students"} ({count})</h2>
     <div>
       {allStudents.map((student) => (
     <Student key={student.id} student={student} />
-  ))};
+  ))}
       </div>
       </>
   )
