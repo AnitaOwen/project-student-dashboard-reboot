dd74e28e3cb0ff6e4f553b24f908448e5508e06f
AnitaOwen <anitaowen@pursuit.org>
2024-01-18 19:15:48 -0500

    started to make a search bar

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,9 +1,30 @@
 import Student from "./Student";
+// import { useState } from "react";
 
 
 const StudentList = ({allStudents, selectedCohort}) => {
-  const count = allStudents.length
+  // const [searchInput, setSearchInput] = useState("")
 
+  // function filterStudents(){
+  //   return allStudents.filter((student)=> {
+  //     const name = student.names
+  //     const fullName = `${name.preferredName} ${name.middleName} ${name.surname}`
+  //     return fullName.toLowerCase().match(searchInput.toLowerCase())
+  //   })
+  // }
+  
+  // function handleTextChange(event){
+  //   const input = event.target.value
+  //   setSearchInput(input)
+  // }
+  
+  // function handleCancel(){
+  //   setSearchInput("")
+  // }
+
+  // const searchResults = filterStudents()
+  
+  const count = allStudents.length
   let cohortTitle;
   if(selectedCohort){
     const titleArray = selectedCohort.split("")
@@ -11,16 +32,34 @@ const StudentList = ({allStudents, selectedCohort}) => {
     const year = titleArray.filter((char) => isNaN(+char) === false).join("")
     cohortTitle = `${season} ${year}`
   }
-
   return (
-    <>
+    <main>
+      <div>
+        <form>
+          <label htmlFor="searchInput">Search by first or last name</label>
+          <input 
+          type="search"
+          id="searchInput"
+          onChange={handleTextChange}
+          value={searchInput} 
+          />
+          <button type="button" onClick={handleCancel}>Cancel</button>
+        </form>
+      </div>
+
+      {/* <div>
+        {searchResults.length > 0 && searchResults.map((result) => (
+        <Student key={result.id} student={result} />
+      ))}
+      </div> */}
+
       <h2>{selectedCohort ? cohortTitle : "All Students"} ({count})</h2>
       <div>
         {allStudents.map((student) => (
       <Student key={student.id} student={student} />
       ))}
       </div>
-    </>
+    </main>
   )
 }
 
