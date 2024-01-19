41d08599300cf1a1664bd968ec6af49c374049ad
AnitaOwen <anitaowen@pursuit.org>
2024-01-18 20:55:46 -0500

    cleaned up commented out code

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -39,37 +39,6 @@ const Aside = ({ handleOnClick }) => {
             <Link to={`/students/${cohort.replace(" ", "")}`}>{cohort}</Link>
           </li>
           ))}
-
-        {/* <li onClick={() => handleOnClick("Winter2025")}>
-          <Link to="/students/Winter2025">Winter 2025</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Spring2025")}>
-          <Link to="/students/Spring2025">Spring 2025</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Summer2025")}>
-          <Link to="/students/Summer2025">Summer 2025</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Fall2025")}>
-          <Link to="/students/Fall2025">Fall 2025</Link>
-        </li>
-        <li onClick={() => handleOnClick("Winter2026")}>
-          <Link to="/students/Winter2026">Winter 2026</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Spring2026")}>
-          <Link to="/students/Spring2026">Spring 2026</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Summer2026")}>
-          <Link to="/students/Summer2026">Summer 2026</Link>
-        </li>
-
-        <li onClick={() => handleOnClick("Fall2026")}>
-          <Link to="/students/Fall2026">Fall 2026</Link>
-        </li> */}
       </ul>
     </div>
   );
