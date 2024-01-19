aa025d0bbe825984acca944744a9981cefadae35
AnitaOwen <anitaowen@pursuit.org>
2024-01-18 20:35:01 -0500

    count and title works with search bar

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -2,32 +2,29 @@ import Student from "./Student";
 import { useState } from "react";
 
 
-const StudentList = ({allStudents, selectedCohort}) => {
+const StudentList = ({ allStudents, selectedCohort }) => {
   const [searchInput, setSearchInput] = useState("")
 
-  function filterStudents(){
-    return allStudents.filter((student)=> {
-      const name = student.names
-      const firstName = name.preferredName
-      const lastName = name.surname
-      const firstNameMatch = firstName.toLowerCase().match(searchInput.toLowerCase())
-      const lastNameMatch = lastName.toLowerCase().match(searchInput.toLowerCase())
-      return firstNameMatch || lastNameMatch
-    })
-  }
-  
   function handleTextChange(event){
     const input = event.target.value
     setSearchInput(input)
   }
   
-  function handleCancel(){
-    setSearchInput("")
+  function filterStudents(){
+    return allStudents.filter((student)=> {
+      const name = student.names
+      const firstNameMatch = name.preferredName.toLowerCase().match(searchInput.toLowerCase())
+      const lastNameMatch = name.surname.toLowerCase().match(searchInput.toLowerCase())
+      const middleNameMatch = name.middleName.toLowerCase().match(searchInput.toLowerCase())
+      return firstNameMatch || lastNameMatch || middleNameMatch
+    })
   }
-
   const searchResults = filterStudents()
   
+  
   const count = allStudents.length
+  // console.log(count)
+
   let cohortTitle;
   if(selectedCohort){
     const titleArray = selectedCohort.split("")
@@ -35,32 +32,28 @@ const StudentList = ({allStudents, selectedCohort}) => {
     const year = titleArray.filter((char) => isNaN(+char) === false).join("")
     cohortTitle = `${season} ${year}`
   }
+
   return (
     <main>
       <div>
         <form>
-          <label htmlFor="searchInput">Search by first or last name</label>
+          <label htmlFor="searchInput">
+            Search {selectedCohort ? cohortTitle : "All Students"}:
+          </label>
+          <div>
           <input 
+          placeholder="Student Name"
           type="search"
           id="searchInput"
           onChange={handleTextChange}
           value={searchInput} 
           />
-          <button type="button" onClick={handleCancel}>Cancel</button>
+          </div>
         </form>
       </div>
-
+      
       <div>
-        {searchResults.length > 0 ? (
-          searchResults.map((result)=>(
-            <Student key={result.id} student={result} />
-          ))) : (
-          <p>No students with the name "{searchInput}" found in this list.</p>
-        )}
-
-      </div>
-      <div>
-        {searchInput.length === 0 && (
+        {searchInput === "" && (
           <>
             <h2>{selectedCohort ? cohortTitle : "All Students"} ({count})</h2>
             <div>
@@ -71,12 +64,16 @@ const StudentList = ({allStudents, selectedCohort}) => {
           </>
         )} 
       </div>
-      {/* <h2>{selectedCohort ? cohortTitle : "All Students"} ({count})</h2>
+
       <div>
-        {allStudents.map((student) => (
-      <Student key={student.id} student={student} />
-      ))}
-      </div> */}
+        {searchResults.length > 0 ? (
+          searchResults.map((result)=>(
+            <Student key={result.id} student={result} />
+          ))) : (
+          <p>No student with a name containing "{searchInput}" found in {selectedCohort ? cohortTitle : "All Students"}.</p>
+        )}
+      </div>
+
     </main>
   )
 }
