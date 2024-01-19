6a08d2244698cf92a91a985c9c801146eca59d0e
Jazon Younge <jay@Jazons-Air.lan>
2024-01-19 12:42:58 -0500

    made basic css and added some classnames

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -4,40 +4,41 @@ import { Link } from "react-router-dom";
 const Student = ({ student, listView }) => {
   const name = student.names;
 
-  const birthday = new Date(student.dob)
+  const birthday = new Date(student.dob);
   const options = {
     month: "long",
     day: "numeric",
-    year: "numeric"
-  }
-  
-  return (
+    year: "numeric",
+  };
 
-    <div>
+  return (
+    <div className="student">
       {listView ? (
         <>
-        <Link to={`student/${student.id}`}>
-          <h3>
-            {name.preferredName} {name.middleName} {name.surname}
-          </h3>
-        </Link>
+          <Link to={`student/${student.id}`}>
+            <h3>
+              {name.preferredName} {name.middleName} {name.surname}
+            </h3>
+          </Link>
         </>
       ) : (
         <>
-        <Link to={`/student/${student.id}`}>
-          <img src={student.profilePhoto} alt={name.preferredName} />
-          <h3>
-            {name.preferredName} {name.middleName} {name.surname}
-          </h3>
-          <p>{student.username}</p>
-          <p>Birthday: {birthday.toLocaleDateString('en-US', options)}</p>
-        </Link>
+          <Link to={`/student/${student.id}`}>
+            <img
+              className="student_image"
+              src={student.profilePhoto}
+              alt={name.preferredName}
+            />
+            <h3>
+              {name.preferredName} {name.middleName} {name.surname}
+            </h3>
+            <p>{student.username}</p>
+            <p>Birthday: {birthday.toLocaleDateString("en-US", options)}</p>
+          </Link>
         </>
       )}
     </div>
-
   );
 };
 
 export default Student;
-
